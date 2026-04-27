4095feea9d0d5c154ea250b54a2effa89a0e9f08
nabeelmohammed0606 <nabeelmohammed0606@gmail.com>
2026-04-27 12:01:05 -0400

    name and age test

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-age = input("Enter your age: ")
-print(age)
+# this is what I am using to learn and test python projects to assist my journey on becoming a devsecops engineer
+
+name = input("Tell me, what is your name: ")
+age = input("Tell me, what is your age: ")
+
+print(f"very nice {name} your are {age}")
