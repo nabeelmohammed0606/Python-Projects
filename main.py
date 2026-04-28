27fa75badbb9f47b0f9892a62383bccef9e4a298
nabeelmohammed0606 <nabeelmohammed0606@gmail.com>
2026-04-28 08:26:22 -0400

    this asks the user user a few personal questions

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # this is what I am using to learn and test python projects to assist my journey on becoming a devsecops engineer
 
-machine = (input("Enter the current machine to store it's respective hours: "))
-hours = float(input("Enter the number of hours on the machine: "))
+color = (input("What is your favourite color? "))
 
-print(f"the {machine} currently has {hours} hours")
+age = (input("What is your age? "))
+
+movie = (input("What is your favourite movie? "))
+
+weight = (input("What is your current weight? "))
+
+siblings = (input("How many siblings do you have? "))
+
+print(f"You are {age} years old and your favourite color is {color}. Your weight is {weight} and you have {siblings} siblings. Your favourite movie is {movie}")
