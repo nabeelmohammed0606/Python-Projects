b9092e2de4d5f5312d59c8fafbfb5bc6efc49cf5
nabeelmohammed0606 <nabeelmohammed0606@gmail.com>
2026-04-28 15:02:52 -0400

    I learned how to use less than and equal too with multiple figures

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 #This branch is used for the questions
 
+#efficiency question
+# formula: efficiency = distance_traveled / fuel_used
+# categorize cars by highly efficient: efficiency greater than 15km/l
+# moderately efficienct: 10km/l and 15km/l (inclusive)
+# inefficient: efficiency less than 10km/l
+
+distance_traveled = float(input("Enter the distance traveled in km:"))
+
+fuel_used = float(input("Enter the amount of fuel used: "))
+
+fuel_efficiency = (distance_traveled / fuel_used)
+
+# used for testing:
+#print(f"{fuel_efficiency}")
+
+if fuel_efficiency > 15: print("Highly Efficient")
+
+elif 10 <= fuel_efficiency <= 15:
+        print("Moderately Efficient")
+
+else: print("Inefficient")
