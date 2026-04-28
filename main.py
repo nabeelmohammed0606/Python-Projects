d2bec55c4505ffbab35f947ac644dc9325b906e0
nabeelmohammed0606 <nabeelmohammed0606@gmail.com>
2026-04-28 09:40:01 -0400

    simple multiplication cash register

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-# this is what I am using to learn and test python projects to assist my journey on becoming a devsecops engineer
+#cash register simple
 
-color = (input("What is your favourite color? "))
+item_price = float(input("Enter the price of the item: "))
+item_quantity = int(input("Enter the quantity of the item: "))
 
-age = (input("What is your age? "))
+total = item_price * item_quantity 
 
-movie = (input("What is your favourite movie? "))
-
-weight = (input("What is your current weight? "))
-
-siblings = (input("How many siblings do you have? "))
-
-print(f"You are {age} years old and your favourite color is {color}. Your weight is {weight} and you have {siblings} siblings. Your favourite movie is {movie}")
+print(f"Total: ${total}")
