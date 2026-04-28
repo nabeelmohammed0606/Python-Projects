d0e0ba7028e51cccf94e106f5f145e5b2b1888dc
nabeelmohammed0606 <nabeelmohammed0606@gmail.com>
2026-04-28 11:30:02 -0400

    area of rectangle

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-#lil problems
+length = float(input("Enter the length of the rectangle :"))
+width = float(input("Enter the widht of the rectangle: "))
 
-age = 25
-result = (age == 40)
-print(result)
-
-VAT = 12.5
-
-# 
-tempreature = 100
-#checking to see if the tempreature is really 100 degrees
-
-result = (tempreature == 100)
-print(f"{result}")
+area = (length * width)
+print(area)
